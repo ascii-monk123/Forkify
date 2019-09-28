7a6234fada5191be687f7865db56bd57279a2096
aahan_5833 <aahancharak@gmail.com>
2019-09-28 17:34:49 +0530

    Fixed the babel bug and added the latest babel pack

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,6 @@
 // Global app controller
 import x from './test';
-const x = 23;
+const l = 23;
 console.log(
-  `I imported ${x} from another modules this is working on live server ${x}`
+  `I imported ${x} from another modules this is working on live server ${l}`
 );
